0eeedc7f6797653ccb670f8f6b5c72b16ef5784d
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2023-05-08 11:08:10 +0530

    Update 0058-length-of-last-word.cs
    New Approach

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,22 @@
-class Solution {
-    public int lengthOfLastWord(String str) {
-        String[] words = str.split(" ");
-        return words.length == 0 ? 0 : words[words.length-1].length();
+class Solution 
+{
+    public int lengthOfLastWord(String str) 
+    {
+        //String[] words = str.split(" ");
+        //return words.length == 0 ? 0 : words[words.length-1].length();
+        string str = s.TrimEnd();
+        int count = 0;
+        for(int i = str.Length-1; i >= 0; i--)
+        {
+            if(str[i] != ' ')
+            {
+                count++;
+            }
+            if(str[i] == ' ')
+            {
+                break;
+            }
+        }
+        return count;
     }
 }
